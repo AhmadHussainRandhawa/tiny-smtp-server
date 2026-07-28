1d93c1fe82a91bf7f578bf5edf16990076e058da
Ahmad Hussain <official.ahmadrandhawa@gmail.com>
2026-07-28 16:26:11 +0500

    feat: accept TCP connections and send SMTP greeting

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net"
+)
 
+func main() {
+	listener, err := net.Listen("tcp", "127.0.0.1:2525")
+	if err != nil {
+		fmt.Println("Failed to start server:", err)
+		return
+	}
+
+	defer listener.Close()
+
+	fmt.Println("SMTP server listening on 127.0.0.1:2525")
+
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println("Failed to accept connection:", err)
+		return
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Client connected:", conn.RemoteAddr())
+
+	fmt.Fprint(conn, "220 localhost Tiny SMTP Server Ready\r\n")
 }
